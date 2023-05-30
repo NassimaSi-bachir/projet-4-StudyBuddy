a5283642001c0dd68b195c58ed3fb55dbaf35e72
NassimaSi-bachir <125278524+NassimaSi-bachir@users.noreply.github.com>
2023-05-30 18:35:36 +0200

    création du user

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{   
+    private $encoder;
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {   
+        $this->encoder = $encoder;
+    }
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('nassima.siba@gmail.com');
+        $user->setPassword($this->encoder->hashPassword($user,'pass'));
+        $user->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}

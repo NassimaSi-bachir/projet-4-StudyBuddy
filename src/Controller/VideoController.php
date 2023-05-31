14819314183a42e4efcd948e3abb0d2841381d29
sidibe-bintou <sidib.bintou@gmail.com>
2023-05-31 14:12:26 +0200

    PAGE SHOW

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -16,8 +16,17 @@ class VideoController extends AbstractController
         $videos = $videoRepository->findAll();
          //On rend la page en lui passant la liste des videos
         return $this->render('video/index.html.twig', [
-            'controller_name' => 'VideoController',
             'videos' => $videos,
         ]);
+    }
+    #[Route('/video/{slug}', name: 'app_video_show')]
+    public function showVideo($slug, VideoRepository $videoRepository):Response
+    {
+       //On récupère le livre correspondant au slug
+        $video = $videoRepository->findOneBy(['slug' => $slug]);
+       //On rend la page en lui passant le livre
+        return $this->render('video/show.html.twig', [
+        'video' => $video,
+        ]);  
     }
 }

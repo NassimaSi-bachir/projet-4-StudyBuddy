dfa0b36f058cc3297e1162229e2939eb6dcd90b3
Habibata-Doucoure <habibata-doucoure@hotmail.fr>
2023-06-01 15:04:40 +0200

    register

--- migrations/Version20230601122302.php
+++ migrations/Version20230601122302.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230601100012 extends AbstractMigration
+final class Version20230601122302 extends AbstractMigration
 {
     public function getDescription(): string
     {
